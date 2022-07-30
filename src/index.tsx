920485dba970ef61656636597a07c028c2682591
MarkNunez97 <41883869+MarkNunez97@users.noreply.github.com>
2022-07-30 11:13:29 -0600

    downloaded font, using it locally

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './fonts/PF-Tempesta-Seven/pf_tempesta_seven.ttf'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
